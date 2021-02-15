9c247ab1f493fdcfb2071cd4df6ac9fd77fcde04
guoliang <g.zhang724@mybvc.ca>
2021-02-15 11:10:31 -0700

    front-end initialize

---
+++ blog-front/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Main from "./components/main";
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
