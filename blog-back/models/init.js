fd528ccc9cf637c62e1a71b51db12f4767bbe600
guoliang <g.zhang724@mybvc.ca>
2021-10-10 14:53:43 +0800

    milestone1

--- blog-back/models/init.js
+++
@@ -1,9 +0,0 @@
-require("./blog");
-require("./user");
-require("./comment");
-const sequelize = require("./db");
-
-//initialize models at once
-sequelize.sync({ alter: true }).then(() => {
-  console.log("all modules are set up");
-});
