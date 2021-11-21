b0f0c7f5d11acffb5dae02a79ac7035fa44b3804
guoliang <guoliang@guoliangdeMacBook-Pro.local>
2021-11-21 12:20:39 +0800

    feat: backend deployed

--- blog-front/src/api/request.js
+++ blog-front/src/api/request.js
@@ -1,7 +1,7 @@
 import { ajax } from "./axios";
 import axios from "axios";
 const dailySentenceApi = "http://v3.wufazhuce.com:8000/api/channel/one/0/0";
-const baseUrl = "";
+const baseUrl = "http://blogs.guoliang831.com/api";
 
 //get daily sentence
 export function getSentence() {
@@ -11,35 +11,35 @@ export function getSentence() {
 
 //get the bloglist
 export function getBlogList() {
-  return ajax("/blog/list");
+  return ajax(baseUrl+ "/blog/list");
 }
 
 // get the bloglist by keyword,tags,page
 export function getBlogListsByparameters(keyword = "", tag = "all", page = 1) {
-  return ajax(`/blog/list/parameter`, { keyword, tag, page }, "post");
+  return ajax(baseUrl+ `/blog/list/parameter`, { keyword, tag, page }, "post");
 }
 
 //create a new blog
 export function createNewBlog(title, content, tags, img) {
-  return ajax("/blog/new", { title, content, tags, img }, "post");
+  return ajax(baseUrl+"/blog/new", { title, content, tags, img }, "post");
 }
 
 //hanle likes
 export function addLikes(id, likes) {
-  return ajax("/blog/likes/add", { id, likes }, "post");
+  return ajax(baseUrl+"/blog/likes/add", { id, likes }, "post");
 }
 //get tags
 export function getTags() {
-  return ajax("/blog/taglist");
+  return ajax(baseUrl+"/blog/taglist");
 }
 
 //add one view on one blog
 export function addOneView(id) {
-  return ajax("/blog/addview", { id }, "post");
+  return ajax(baseUrl+"/blog/addview", { id }, "post");
 }
 
 /*--------------vertify username and password----------------------- */
 
 export function getUserInfo(username, password) {
-  return ajax("/login", { username, password }, "post");
+  return ajax(baseUrl+"/login", { username, password }, "post");
 }
